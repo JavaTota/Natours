73713b7cf0dd8d15abf5d5f2f4bfaf153420acc9
JavaTota <jomigone@gmail.com>
2025-07-03 10:45:23 -0500

    Update dependencies and enhance Mapbox integration in views
    - Downgrade helmet package from 8.1.0 to 3.16.0 for compatibility
    - Add new Mapbox script and styles to tour view
    - Update base template to include Mapbox script
    - Modify tour view to pass locations data to the map

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,1 +1,47 @@
-console.log('hello');
+/*eslint-disable*/
+
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+
+console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiamFodmEiLCJhIjoiY21jbmg1YnhxMDdpZDJqb2x2bzdneWh1aSJ9.kmSuxTCVvUHlqHgoe5lf6g';
+
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/mapbox/light-v10', // style URL
+  //   center: [-118.113491, 34.111745],
+  //   zoom: 5,
+  //   interactive: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day : ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});
