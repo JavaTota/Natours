72c8a6c4f47356c09501fe6519d65a8a89646487
JavaTota <jomigone@gmail.com>
2025-07-02 22:02:29 -0500

    Enhance tour view by setting dynamic title; refactor base.pug for head block; add Mapbox script to tour view; create mapbox.js for future map integration.

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -23,6 +23,7 @@ exports.getTour = catchAsync(async (req, res) => {
 
   //3)RENDER TEMPLATE USING DATA FROM 1)
   res.status(200).render('tour', {
+    title: `${tour.name} Tour`,
     tour,
   });
 });
