fe29c1cf4f65a3c45e1ace76b9db0c3e06fd0f86
JavaTota <jomigone@gmail.com>
2025-07-21 14:41:51 -0500

    feat: integrate Stripe for booking functionality
    - Updated stripe.js to handle checkout session retrieval and error handling with user alerts.
    - Modified viewRoutes.js to include createBookingCheckout middleware for the overview route.
    - Added a link to 'My bookings' in the account.pug to navigate to the new my-tours route.
    - Included Stripe.js script in base.pug for payment processing.
    - Removed duplicate Stripe.js script inclusion from tour.pug.
    - Created bookingModel.js to define the Booking schema with necessary fields and pre-find population.

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModels');
 const User = require('../models/userModel');
+const Booking = require('../models/bookingModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
@@ -59,5 +60,19 @@ exports.updateUserData = catchAsync(async (req, res, next) => {
   res.status(200).render('account', {
     title: 'Your account',
     user: updatedUser,
+  });
+});
+
+exports.getMyTours = catchAsync(async (req, res, next) => {
+  //1) FIND ALL BOOKINGS
+  const bookings = await Booking.find({ user: req.user.id });
+
+  //2) FIND TOURS WITH THE RETURNED IDS
+  const tourIds = bookings.map((el) => el.tour);
+  const tours = await Tour.find({ _id: { $in: tourIds } });
+
+  res.status(200).render('overview', {
+    title: 'My Tours',
+    tours,
   });
 });
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,16 +1,26 @@
 /*eslint-disable*/
 
 import axios from 'axios';
+import { showAlert } from './alert';
+
 const stripe = Stripe(
   'pk_test_51RkrXBHCK3eBVf7HnmvJMyZZ3CQO8dJmyfzw6T4fNYA6cmBUqhFi9O8dStf45It2eP9rF85KxJA0o0x09sFhNwUj00K6dY2t6J',
 );
 
 export const bookTour = async (tourId) => {
-  //1) GET CHECKOUT SESSION FROM API
-  const session = await axios(
-    `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`,
-  );
-  console.log(session);
+  try {
+    //1) GET CHECKOUT SESSION FROM API
+    const session = await axios(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`,
+    );
+    console.log(session);
 
-  //2) CREATE CHECKOUT FORM + CHARGE CREDIT CARD
+    //2) CREATE CHECKOUT FORM + CHARGE CREDIT CARD
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (error) {
+    console.log(error);
+    showAlert('error', error.response?.data?.message || error.message);
+  }
 };
