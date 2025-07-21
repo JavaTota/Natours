3142d501f4667e112692a00222db490013fdde73
JavaTota <jomigone@gmail.com>
2025-07-21 15:11:07 -0500

    feat: add booking CRUD operations to booking routes

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const { protect, restrictTo } = require('../controllers/authController');
-const { getCheckoutSession } = require('../controllers/bookingController');
+const {
+  getCheckoutSession,
+  getAllBooking,
+  createBooking,
+  getOneBooking,
+  updateBooking,
+  deleteBooking,
+} = require('../controllers/bookingController');
 
 const router = express.Router();
 
+router.use(protect);
+
 router.get('/checkout-session/:tourID', protect, getCheckoutSession);
+
+router.use(restrictTo('admin', 'lead-guide'));
+
+router.route('/').get(getAllBooking).post(createBooking);
+
+router
+  .route('/:id')
+  .get(getOneBooking)
+  .patch(updateBooking)
+  .delete(deleteBooking);
 
 module.exports = router;
